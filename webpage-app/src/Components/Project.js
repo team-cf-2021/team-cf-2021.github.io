b4a4e61a27a3e55f949831cb59a974d6e3a849fb
DylanSpence-git <DylanSpence-git@users.noreply.github.com>
2021-07-25 11:54:54 -0700

    aletered text

--- webpage-app/src/Components/Project.js
+++ webpage-app/src/Components/Project.js
@@ -4,27 +4,21 @@ import { Row, Col, Container, Image } from 'react-bootstrap'
 const Project = () => {  
 
     const objectiveTxt = () => (
-        <div id='big-txt'>
+        < /* id='big-txt' */>
         <p>
-            The purpose of the NCDI 2.0 is to expand on the functionality of the current web application and 
-            improve error feedback. The goal is to expand its functionalities which include gift certificate 
-            importation and increased accuracy in data validation and date recognition.
+            The purpose of the NCDI 2.0 is to expand on the functionality of the current web application allowing to user to import gift certificates 
+            and guest information, improve user feedback for invalid data, and allow the user to update all existing items in their account. 
         </p>
-        </div>
+        </>
     )
 
     const situationTxt = () => (
         <>
         <p>
-            Checkfront receives bulk booking data from their clients which can widely vary from client to client. 
+            Checkfront receives bulk booking data from their clients which can widely vary in both format and data type from client to client. 
             They seek to streamline the process of importing large quantities of booking information so that 
             Checkfront can save time, resources, and also improve turn-around for their clients. This project will 
             increase ease-of-use for both the technical and non-technical Checkfront team members. 
-        </p>
-        <p>
-            Our tasks with the New Customer Data Importer (NCDI) 2.0 are to improve the error handling, enhance data 
-            recognition and validation, and add new functionality allowing users to: export and remap account information, 
-            import gift certificates, and import guest information.  
         </p>
         </>
     )
@@ -34,19 +28,18 @@ const Project = () => {
         <Row>
             <Col lg={6}>
                 <p>
-                    The NCDI 2.0 deliverables consist of adding the ability to: import gift certificates, import guest 
-                    information, and export and remap booking information. The project scope includes the following:
+                    The NCDI 2.0 deliverables consist of adding the the following features: import gift certificates, import guest 
+                    information, and update all items in the users account. 
                 </p>
                 <br/>
+                <p>The project scope also includes the following:</p>
                 <ul>
                     <li>Expand the accuracy of data validation.</li>
                     <li>Fix the absence of creation date validation.</li>
+                    <li>Enhance data recognition.</li>
                     <li>Audit the error-handling and field type recognition.</li>
                     <li>Handle duplicate bookings.</li>
                     <li>Improve the date recognition.</li>
-                    <li>Add gift certificate import functionality.</li>
-                    <li>Add the ability to re-import bookings and remap the data.</li>
-                    <li>Add guest information import functionality.</li>
                 </ul>
                 <br/>
             </Col>
@@ -58,7 +51,7 @@ const Project = () => {
     )
 
     const logisticsTxt = () => (
-        <div id='big-txt'>
+        <div /*  id='big-txt' */>
         <p>
             Management of the NCDI’s tasks is performed using Jira (An issue and project tracking software). 
             The project's source code is hosted on Github providing a remote repository with version control. 
@@ -79,17 +72,17 @@ const Project = () => {
                 <Col id='features-col'>
                     <Image src='../Images/import-bookings.png' id='col-img' fluid /> 
                     <p />
-                    Import CSV files of bookings, gift certificates, and guest information.
+                    Import CSV files containing either booking, gift certificate, or guest information.
                 </Col>
                 <Col id='features-col'>
                     <Image src='../Images/map-items.png' id='col-img' fluid />  
                     <p />
-                    Map items using the dropdown menus.
+                    Map items to headers contained in the CSV file using the dropdown menus.
                 </Col>
                 <Col id='features-col'>
                     <Image src='../Images/duplicate-bookings.png' id='col-img' fluid />  
                     <p />
-                    Option of removing duplicate bookings before importing.
+                    Menu displaying all duplicates found allowing removal before importing.
                 </Col>            
             </Row>                
 
